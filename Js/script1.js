03b48700063c9357b403093bd247c3c8c3cfe3b2
Al Ameen N <alameen.n@experionglobal.com>
2022-11-05 08:21:36 +0530

    ameen: home pg count down

--- Js/script1.js
+++ Js/script1.js
@@ -15,3 +15,34 @@ menu.addEventListener('click', () =>{
     navbar.classList.toggle('active');
 });
 
+let countDate = new Date('nov 6, 2022 00:12:00').getTime();
+
+function countDown(){
+    let now = new Date().getTime();
+    gap = countDate - now;
+
+    let second = 1000;
+    let minute = second * 60;
+    let hour = minute * 60;
+    let day = hour * 24;
+    
+    let d = Math.floor(gap/(day));
+    let h = Math.floor((gap%(day))/(hour));
+    let m = Math.floor((gap%(hour))/(minute));
+    let s = Math.floor((gap%(minute))/(second));
+
+    document.getElementById('day').innerText = d;
+    document.getElementById('hour').innerText = h;
+    document.getElementById('min').innerText = m;
+    document.getElementById('second').innerText = s;
+}
+
+
+setInterval(countDown, 1000)
+
+// if (gap<0)
+// clearInterval(shibu)
+
+// if(gap<0){
+//     clearInterval(interval);
+// }
